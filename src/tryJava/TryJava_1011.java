717cee3b04effe9fbdebedbe29a4f4fe28786e8b
github KeitaUenishi <keita.uenishi@gmail.com>
2020-10-11 09:25:02 +0900

    Java問題  N 週間後が何日後かを表示する機能

---
+++ src/tryJava/TryJava_1011.java
@@ -0,0 +1,29 @@
+package tryJava;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryJava_1011 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Scanner scanner = new Scanner(System.in);
+		int afterWeek = 0;
+
+		try {
+			afterWeek = scanner.nextInt();
+
+		} catch (InputMismatchException e) {
+			// TODO: 数値以外が入力されたとき
+			System.out.println("正しい数値を入力してください");
+			System.out.println(e);
+		}
+
+		int afterDay = afterWeek * 7;
+
+		System.out.println(afterDay);
+
+	}
+
+}

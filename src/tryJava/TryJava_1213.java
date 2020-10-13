2cfd4b5b74adeec0dca283de66a05c34c6898cba
github KeitaUenishi <keita.uenishi@gmail.com>
2020-10-13 10:14:10 +0900

    入力された整数回分の文字列を入力し、その長さを数値で返す

---
+++ src/tryJava/TryJava_1213.java
@@ -0,0 +1,22 @@
+package tryJava;
+
+import java.util.Scanner;
+
+public class TryJava_1213 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Scanner scanner = new Scanner(System.in);
+		int count = scanner.nextInt();
+		String number;
+
+		for (int i = 0; i < count; i++) {
+
+			number = scanner.next();
+			System.out.println(number.length());
+		}
+
+	}
+
+}

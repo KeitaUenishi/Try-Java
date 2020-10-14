cd3f3b054579e00a7d1ee56ca3b00805d235806e
github KeitaUenishi <keita.uenishi@gmail.com>
2020-10-14 10:10:20 +0900

    入寮された数字をもとに合格判定する処理

---
+++ src/tryJava/TryJava1014.java
@@ -0,0 +1,26 @@
+package tryJava;
+
+import java.util.Scanner;
+
+public class TryJava1014 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Scanner scanner = new Scanner(System.in);
+		int point = scanner.nextInt();
+
+		if (80 <= point && point <= 100) {
+			System.out.println("pass");
+
+		} else if (0 <= point && point < 80) {
+			System.out.println("fail");
+
+		} else {
+			System.out.println("0～100の数値を入力してください");
+		}
+		scanner.close();
+
+	}
+
+}

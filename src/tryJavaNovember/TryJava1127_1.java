d6ec17b9c0331a2d5a7c29d83cad2e203b364566
github KeitaUenishi <keita.uenishi@gmail.com>
2020-11-27 18:38:34 +0900

    Paiza問題

--- src/tryJavaNovember/TryJava1127_1.java
+++ src/tryJavaNovember/TryJava1127_1.java
@@ -2,12 +2,19 @@ package tryJavaNovember;
 
 import java.util.Scanner;
 
-public class TryJava1126_1 {
+public class TryJava1127_1 {
 
 	public static void main(String[] args) {
 
 		/**
-		 * 二次元配列練習
+		 * 11/27 解けず 後日再挑戦
+		 *
+		 * 5行5列の五目並べの盤面が与えられます。
+		 * 盤面の各マスには、"O"か"X"か"."が書かれています。
+		 * "O"と"X"は、それぞれプレイヤーの記号を表します。
+		 * 同じ記号が縦か横か斜めに連続で5つ並んでいれば、その記号のプレイヤーが勝者となります。
+		 * 勝者の記号を1行で表示してください。
+		 * 勝者がいない場合は、引き分けとして、"D"を表示してください。
 		 */
 
 		Scanner scanner = new Scanner(System.in);
@@ -16,6 +23,11 @@ public class TryJava1126_1 {
 
 		for (int i = 0; i < 5; i++) {
 			board[i] = scanner.next().split("");
+			//横の判定
+			String diaSide = judgement(diagSide(board));
+			if (!diaSide.equals("D")) {
+				result = diaSide;
+			}
 		}
 
 		// ／の判定
@@ -28,6 +40,12 @@ public class TryJava1126_1 {
 		String diagL = judgement(diagLeftUp(board));
 		if (!diagL.equals("D")) {
 			result = diagL;
+		}
+
+		// 縦の判定
+		String diaVer = judgement(diagVer(board));
+		if (!diaVer.equals("D")) {
+			result = diaVer;
 		}
 
 		System.out.println(result);
@@ -52,6 +70,24 @@ public class TryJava1126_1 {
 		for (int i = 0; i < 5; i++) {
 			line += board[i][i];
 		}
+		return line;
+	}
+
+	// 縦方向の並びをStringで返すメソッド
+	static String diagVer(String[][] board) {
+		String line = "";
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				line += board[j][i];
+			}
+		}
+		return line;
+	}
+
+	// 横方向の並びをStringで返すメソッド
+	static String diagSide(String[][] board) {
+		String line = "";
+		line += board;
 		return line;
 	}
 

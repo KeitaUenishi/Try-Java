b9d30972e5f804e75e4ca6bcaeb14b3d770d585d
github KeitaUenishi <keita.uenishi@gmail.com>
2020-11-28 22:00:43 +0900

    Paiza問題

---
+++ src/tryJavaNovember/TryJava1128_1.java
@@ -0,0 +1,15 @@
+package tryJavaNovember;
+
+import java.util.Scanner;
+
+public class TryJava1128_1 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		System.out.println((180 * (n - 2)) / n);
+
+	}
+}
